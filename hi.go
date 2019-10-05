16b6f998dd863e8bba24e7a1eefdc7dcea33d09b
D1N35H <37468808+D1N35H@users.noreply.github.com>
2019-10-05 13:15:22 +0800

    Create hi.go

---
+++ hi.go
@@ -0,0 +1,8 @@
+package a
+var TextInGitHub = "This message is coming from my github repository from Package A."
+
+package b
+var TextInGitHub = "This message is coming from my github repository from Package B."
+
+package c
+var TextInGitHub = "This message is coming from my github repository from Package C."
